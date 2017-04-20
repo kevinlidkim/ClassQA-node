d277209c4856e663b219cce71573a37e1040bcdb
Yong Lei <will19ie95@gmail.com>
2017-04-19 23:51:37 -0400

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('myApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'UserCtrl', 'ClassCtrl', 'QaCtrl' ,'MainServ', 'UserServ', 'ClassServ', 'QaServ','angularMoment', 'vcRecaptcha']);
+angular.module('myApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'UserCtrl', 'ClassCtrl', 'QaCtrl' ,'MainServ', 'UserServ', 'ClassServ', 'QaServ', 'angularMoment', 'vcRecaptcha']);
